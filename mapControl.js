9d7e175230956b9745f5b75b0975c6174cbd4745
Kevin_Lin <30970763+WHULS@users.noreply.github.com>
2018-05-04 01:51:13 +0800

    Add files via upload

--- mapControl.js
+++ mapControl.js
@@ -1,23 +1,3 @@
-var map = new ol.Map({
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ],
-    target: 'map',
-    controls: ol.control.defaults({
-        attributionOptions: {
-            collapsible: false
-        }
-    }),
-    view: new ol.View({
-        projection:'EPSG:4326', //坐标系为WGS84
-        //projection:'EPSG:3857', //OSM默认坐标系
-        center: [114.36, 30.54], //中心为武汉大学
-        zoom: 15
-    })
-});
-
 document.getElementById('zoom-out').onclick = function() {
     var view = map.getView();
     var zoom = view.getZoom();
@@ -68,7 +48,7 @@ document.getElementById('move-down').onclick = function () {
 
 document.getElementById('move-to-WHU').onclick = function () {
     var view = map.getView();
-    view.setCenter([114.36,30.54]);
+    view.setCenter([114.36, 30.54]);
     view.setZoom(15);
     map.render();
 }
@@ -78,3 +58,67 @@ document.getElementById('get-center').onclick = function () {
     var center = view.getCenter();
     alert(center);
 }
+
+document.getElementById('move-to-CHINA').onclick = function () {
+    var view = map.getView();
+    view.setCenter([106.75, 33.60]);
+    view.setZoom(5);
+    map.render();
+}
+
+var XiaoGan = new ol.Feature({
+   geometry: new ol.geom.Point([113.9069, 30.9184])
+});
+var ZhuZhou = new ol.Feature({
+    geometry: new ol.geom.Point([113.1482, 27.8395])
+});
+var Tianjin = new ol.Feature({
+    geometry: new ol.geom.Point([117.2092, 39.1028])
+});
+var GanZhou = new ol.Feature({
+    geometry: new ol.geom.Point([114.922, 25.869])
+});
+var ZhangZhou = new ol.Feature({
+    geometry: new ol.geom.Point([117.5373, 25.0016])
+});
+
+//设置图标
+var dogeStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+        crossOrigin: 'anonymous',
+        src: 'image/doge.png'
+    })
+});
+GanZhou.setStyle(dogeStyle);
+Tianjin.setStyle(dogeStyle);
+XiaoGan.setStyle(dogeStyle);
+ZhangZhou.setStyle(dogeStyle);
+ZhuZhou.setStyle(dogeStyle);
+
+//包含图标的矢量图层
+var vectorSource = new ol.source.Vector({
+    features: [XiaoGan, Tianjin, ZhuZhou, GanZhou, ZhangZhou]
+});
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+});
+//OSM地图层
+var osmLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+var map = new ol.Map({
+    layers: [osmLayer, vectorLayer],
+    target: document.getElementById('map'),
+    controls: ol.control.defaults({
+        attributionOptions: {
+            collapsible: false
+        }
+    }),
+    view: new ol.View({
+        projection:'EPSG:4326', //坐标系为WGS84
+        //projection:'EPSG:3857', //OSM默认坐标系
+        center: [106.75, 33.60], //中心
+        zoom: 5
+    })
+});
